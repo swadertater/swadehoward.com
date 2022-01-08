a4828bac0ce6f936317a39c61e588bab2c9f6aa6
Swade Howard <swade.howard@gmail.com>
2022-01-08 15:26:14 -0800

    update introduction

--- src/introduction/Introduction.tsx
+++ src/introduction/Introduction.tsx
@@ -1,22 +1,20 @@
 import React, { ReactElement } from "react";
-import './Introduction.css'
+import "./Introduction.css";
 
 const Introduction = (): ReactElement => {
   return (
     <div className="introduction">
-      I grew up in southern Illinois playing a lot of video games. I was
-      introduced to Counter Strike and PC gaming by one my foreign exchange
-      student brother while I was in middle school. I kept this interest for years to come and as
-      games increased their graphics and demand for computing power, I could no
-      longer use my mom's work laptop to play my favorite FPS games. After many
-      YouTube videos and earning some money by working for my dad's taxidermy
-      shop, I bought a PC from BestBuy and an Nvidia 550 EVGA graphics card.
-      This sparked my interest in computers and kicked-started my technology
-      adventure. Soon after, I was soon fixing computers and smartphones for locals. When
-      college approached and I had to focus on a degree, I decided I was done
-      with hardware and wanted to learn how to build software. Four and a half
-      years later, I graduated with a Bachelor's in Computer Science with four
-      internships under my belt.
+      Born and raised in southern Illinois. Grew up playing a lot of video games
+      and baseball. Attended Southern Illinois Unviersity Edwardsville from
+      <span className="text-nowrap">2016 - 2020.</span> During my undergrad, I
+      did an internship at Hunter Engineering. Taught some web development at
+      LaunchCode. Did and internship at World Wide Technology. Did an internship
+      at Less Annoying CRM. Did an internship at ArchitectNow. Currently working
+      as a software engineer (basically just a full-stack web-dev position) at
+      ArchitectNow. In my free time you can catch me at the gym trying to hit
+      365 on deadlifts, on Steam playing some video games, or in the kitchen
+      cooking (probably making an Indian dish because it's the best cuisine to
+      ever exist).
     </div>
   );
 };
